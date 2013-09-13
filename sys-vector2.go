4208a42e14a8b115fa4c934703378c8f20424cdb
Theodore DeRego <tderego94@gmail.com>
2013-09-12 15:09:19 -1000

    Added setters for Vector2

--- sys-vector2.go
+++ sys-vector2.go
@@ -1,5 +1,5 @@
-// Copyright 2012.  All rights reserved. 
-// Use of this source code is governed by a BSD-style  
+// Copyright 2012.  All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 ////////////////////////////////////////////////////////////
@@ -26,7 +26,6 @@
 //
 ////////////////////////////////////////////////////////////
 package sfml
-
 
 // #cgo LDFLAGS:-lcsfml-system
 // #include <SFML/System.h>
@@ -61,6 +60,8 @@ type (
 	}
 )
 
+// Vector2u ####################################################################
+
 func NewVector2u(x, y uint) Vector2u {
 	return Vector2u{C._NewVector2u(C.uint(x), C.uint(y))}
 }
@@ -73,6 +74,16 @@ func (self Vector2u) Y() uint {
 	return uint(self.Cref.y)
 }
 
+func (self *Vector2u) SetX(x uint) {
+	self.Cref.x = C.uint(x)
+}
+
+func (self *Vector2u) SetY(y uint) {
+	self.Cref.y = C.uint(y)
+}
+
+// Vector2i ####################################################################
+
 func NewVector2i(x, y int) Vector2i {
 	return Vector2i{C._NewVector2i(C.int(x), C.int(y))}
 }
@@ -83,16 +94,34 @@ func (self Vector2i) Y() int {
 	return int(self.Cref.y)
 }
 
+func (self *Vector2i) SetX(x int) {
+	self.Cref.x = C.int(x)
+}
+
+func (self *Vector2i) SetY(y int) {
+	self.Cref.y = C.int(y)
+}
+
+// Vector2f ####################################################################
+
 func NewVector2f(x, y float32) Vector2f {
 	return Vector2f{C._NewVector2f(C.float(x), C.float(y))}
 }
 
-func (self Vector2f) X() float64 {
-	return float64(self.Cref.x)
+func (self Vector2f) X() float32 {
+	return float32(self.Cref.x)
 }
 
-func (self Vector2f) Y() float64 {
-	return float64(self.Cref.y)
+func (self Vector2f) Y() float32 {
+	return float32(self.Cref.y)
+}
+
+func (self *Vector2f) SetX(x float32) {
+	self.Cref.x = C.float(x)
+}
+
+func (self *Vector2f) SetY(y float32) {
+	self.Cref.y = C.float(y)
 }
 
 // internal
